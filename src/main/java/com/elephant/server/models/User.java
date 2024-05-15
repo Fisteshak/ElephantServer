605704b1dcc67cefb18bd6db0b0b4da68335da0e
Fisteshak <antonlazarev2005@gmail.com>
2024-05-15 14:35:05 +0300

    remove message functionality

--- src/main/java/com/elephant/server/models/User.java
+++ src/main/java/com/elephant/server/models/User.java
@@ -3,12 +3,17 @@ package com.elephant.server.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_gen")
     @SequenceGenerator(name = "users_id_gen", sequenceName = "users_id_seq", allocationSize = 1)
@@ -18,11 +23,36 @@ public class User {
     @Column(name = "username", nullable = false, length = 50)
     private String username;
 
-    //password should be encrypted with bcrypt and have {bcrypt} prefix
+    //password should have {prefix} of used hashing algorithm, for example {bcrypt},
+    //see https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/crypto/password/DelegatingPasswordEncoder.html
     @Column(name = "password", nullable = false, length = 500)
     private String password;
 
     @Column(name = "role", nullable = false, length = 50)
     private String role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
